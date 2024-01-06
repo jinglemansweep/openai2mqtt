3884680ac1ec6a8b939685fac1414533da7f88ed
Louis King <jinglemansweep@gmail.com>
2024-01-06 18:23:00 +0000

    chore: added startup logging

--- openai2mqtt/__main__.py
+++ openai2mqtt/__main__.py
@@ -9,4 +9,9 @@ from .utils.logger import setup_logger
 setup_logger(debug=settings.general.debug)
 logger = logging.getLogger(AppMetadata.NAME)
 
-logger.info("HELLO")
+# Startup
+logger.info(f"{AppMetadata.TITLE} v{AppMetadata.VERSION}")
+logger.info(f"Debug: {settings.general.debug}")
+logger.info(
+    f"MQTT: Host={settings.mqtt.host}:{settings.mqtt.port} User={settings.mqtt.username}, Topic={settings.mqtt.topic_prefix}/#)"
+)
